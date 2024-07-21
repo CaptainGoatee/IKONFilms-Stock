13a2a750bb869e918aba3e34e56ceab8c7032905
CaptainGoatee <121584207+CaptainGoatee@users.noreply.github.com>
2024-07-21 18:06:26 +0100

    removed auth for create account

--- app.js
+++ app.js
@@ -145,9 +145,6 @@ app.get("/scan-in", (req, res) => {
   res.render("scan-in", { user: req.user });
 });
 app.get("/create_account", (req, res) => {
-  if (req.isAuthenticated()) {
-    return res.redirect("/home");
-  }
   res.render("signup", {
     error:
       "System: Please create an account. Once you've created one, Please contact a System Administrator to finish setting your account up.",
